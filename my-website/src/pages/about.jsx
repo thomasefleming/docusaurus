9a239465a96fb31ac33398e5d0d897525c85fe16
Thomas E Fleming <thomasefleming@gmail.com>
2024-09-18 20:33:17 -0400

    Create about.jsx

---
+++ my-website/src/pages/about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+function About() {
+  return (
+    <Layout title="About Us">
+      <div style={{ padding: "20px" }}>
+        <h1>About Us</h1>
+        <p>
+   Thomas E Fleming Sandbox   </p>
+      </div>
+    </Layout>
+  );
+}
+
+export default About;
