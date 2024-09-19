036d8c237fd501e72ec53ad61f521d5d18a4aa03
thomasefleming <thomasefleming@gmail.com>
2024-09-18 21:39:28 -0400

    Update docusaurus.config.js

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://thomasefleming.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-website/',
+  baseUrl: '/docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
